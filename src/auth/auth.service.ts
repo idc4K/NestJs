cfd5fcd6dabd3c5822b23b4132c47fe1c23ce527
idc4k <christikonga83@gmail.com>
2023-03-23 00:56:09 +0000

    Exception error

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,7 +2,7 @@ import { ForbiddenException, Injectable } from "@nestjs/common";
 import { AuthDto } from "src/dto";
 import { PrismaService } from "src/prisma/prisma.service";
 import * as argon from 'argon2'
-import { PrismaClientKnownRequestError } from "@prisma/client/runtime";
+import { Prisma } from "@prisma/client";
 
 @Injectable({})
 export class AuthService{
@@ -13,35 +13,36 @@ export class AuthService{
         const hash = await argon.hash(dto.password)
         //Save new user in db
         try{
-        const user = await this.prisma.user.create({
-            data:{
-                firstName:dto.firstName,
-                email:dto.email,
-                hash,
-            },
-            select:{
-                id : true,
-                firstName : true,
-                email:true,
-                createdAt:true
-            }
-            // delete user.hash;
-        })
-
-        //Return the save user
-        return user
-      }
-      catch(error){
-        if(error instanceof PrismaClientKnownRequestError){
-            if(error.code === 'P2002'){
-                throw new ForbiddenException('credentials taken')
-
-            }
+            const user = await this.prisma.user.create({
+                data:{
+                        firstName:dto.firstName,
+                        email:dto.email,
+                        hash,
+                },
+                select:{
+                    id : true,
+                    firstName : true,
+                    email:true,
+                    createdAt:true
+                }
+                // delete user.hash;
+                })
+        
+                //Return the save user
+                return user;
         }
-        throw error
-      }
-     
+        catch (e) {
+            if (e instanceof Prisma.PrismaClientKnownRequestError) {
+              // The .code property can be accessed in a type-safe manner
+              if (e.code === 'P2002') {
+                 throw new ForbiddenException(
+                    'Credentials taken',
+                );
+              }
+            }
+            throw e
     }
+}
     login(){
         return "I'm singnin in service";
     }
